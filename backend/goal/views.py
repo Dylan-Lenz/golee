0799954ffac9673140d6caf3b8211b5e315d166c
unknown <dylanjlenz@gmail.com>
2022-12-28 15:15:02 -0600

    Created POST Function in Views + Tested All Endpoints in PostMan

--- backend/goal/views.py
+++ backend/goal/views.py
@@ -1,15 +1,21 @@
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view, permission_classes
 from goal.models import Goal
 from goal.serializers import GoalSerializer
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 @permission_classes([IsAuthenticated])
 def goals(request):
-   if request.method == 'GET':
+    if request.method == 'GET':
         goals = Goal.objects.filter(user_id=request.user.id)
         serializer = GoalSerializer(goals, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = GoalSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user_id=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
